4b74f3b189bf4d809fe1b5a47b8809a7cb7bdb8a
7h3-F1r3f0x <firefoxaxis@gmail.com>
2018-11-23 17:57:11 +0100

    app changes

--- Android app/IoLego2/app/src/main/java/com/iolego/io_lego/MainActivity.java
+++ Android app/IoLego2/app/src/main/java/com/iolego/io_lego/MainActivity.java
@@ -16,10 +16,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -31,24 +31,33 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "BTConnectionActivity";
     private static String MAC_ADDRESS = null;
     private static final String ID_LEGO = "EV3";
+
+
+    private static final int VOLTAGE_LOW = 20;
+    private static final int VOLTAGE_MIDDLE = 50;
+
+    private static final int CHECK = 100;
+    private static final int END = 255;
+
+    private int numberOfCells;
+    private int checkedCells;
+
     private ImageButton b;
-    private View fails;
+    private ImageView bluetoothImage, batteryImage;
+    private View robotPlaceBtn, progressLayout;
     private int map[][];
     private int x,y;
     public Dialog colorChooseDialog, reconnectDialog;
     private BluetoothSocket socket;
     private InputStream inputStream;
     private OutputStream outputStream;
-    private Button send;
-    private EditText input;
     private boolean robot=false;
-
+    private ProgressBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main_refactor);
 
         map=new int[5][5];
         for(int i=0;i<5;i++){
@@ -57,13 +66,19 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+
         colorChooseDialog=new Dialog(this);
         colorChooseDialog.setContentView(R.layout.choose_color);
         colorChooseDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        fails = colorChooseDialog.findViewById(R.id.robot);
+        robotPlaceBtn = colorChooseDialog.findViewById(R.id.robot);
         reconnectDialog=new Dialog(this);
-        ProgressBar bar=findViewById(R.id.prog_bar);
-        bar.setProgress(10);
+        bar=findViewById(R.id.prog_bar);
+
+        bluetoothImage = findViewById(R.id.bluetoothImg);
+        batteryImage = findViewById(R.id.batteryImg);
+        progressLayout = findViewById(R.id.relProgbar);
+
+        progressLayout.setVisibility(View.INVISIBLE);
 
         /*
         createDialog();
@@ -71,8 +86,9 @@ public class MainActivity extends AppCompatActivity {
 
         /*
         connect();
-
         */
+
+        // batteryInfo();
     }
 
     private BluetoothDevice searchDevice(Set<BluetoothDevice> devices) {
@@ -97,7 +113,11 @@ public class MainActivity extends AppCompatActivity {
                         Log.w(TAG, "Device connected. MAC " + MAC_ADDRESS);
                         outputStream = socket.getOutputStream();
                         inputStream = socket.getInputStream();
+
+                        bluetoothImage.setImageDrawable(getDrawable(R.drawable.ic_bluetooth_connected));
                     } catch(IOException e) {
+                        bluetoothImage.setImageDrawable(getDrawable(R.drawable.ic_bluetooth_disabled));
+
                         createDialog();
                         reconnectDialog.show();
                     }
@@ -112,8 +132,11 @@ public class MainActivity extends AppCompatActivity {
                             Log.w(TAG, "Device connected. MAC " + MAC_ADDRESS);
                             outputStream = socket.getOutputStream();
                             inputStream = socket.getInputStream();
+
+                            bluetoothImage.setImageDrawable(getDrawable(R.drawable.ic_bluetooth_connected));
                         } catch (IOException e) {
-                            //reconnectDialog.show();
+                            bluetoothImage.setImageDrawable(getDrawable(R.drawable.ic_bluetooth_disabled));
+
                             createDialog();
                             reconnectDialog.show();
                         }
@@ -124,15 +147,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             } else {
                 Log.e(TAG, "Bluetooth is disabled.");
+                bluetoothImage.setImageDrawable(getDrawable(R.drawable.ic_bluetooth_disabled));
                 enableBluetooth();
             }
         }
         return false;
     }
 
-    public void reConnect(View view) {
-        connect();
-    }
 
     private void enableBluetooth() {
         try {
@@ -144,6 +165,32 @@ public class MainActivity extends AppCompatActivity {
         startActivity(enabelBT);
         createDialog();
         reconnectDialog.show();
+    }
+
+    public void createDialog() {
+        AlertDialog.Builder build;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            build = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            build = new AlertDialog.Builder(this);
+        }
+        build.setCancelable(false);
+        build.setTitle(R.string.alert_reconnect_title);
+        build.setMessage(R.string.alert_reconnect_text);
+        build.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                connect();
+            }
+        });
+        build.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.exit(0);
+            }
+        });
+
+        reconnectDialog = build.create();
     }
 
     public void chooseColor(View view) {
@@ -176,6 +223,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("color","green in "+x+y);
                 map[x][y]=3;
                 break;
+
             case "red":
                 b.setBackgroundColor(ContextCompat.getColor(this, R.color.red));
                 Log.d("color", "red in "+x+y);
@@ -186,72 +234,111 @@ public class MainActivity extends AppCompatActivity {
                 b.setBackgroundColor(ContextCompat.getColor(this, R.color.light_grey));
                 Log.d("color", "undo in "+x+y);
                 if(map[x][y]==-1){
-                    fails.setVisibility(View.VISIBLE);
+                    robotPlaceBtn.setVisibility(View.VISIBLE);
                     robot=false;
                 }
                 map[x][y]=0;
                 break;
+
             case "robot":
                 if(!robot) {
                     b.setBackgroundColor(ContextCompat.getColor(this, R.color.black));
                     Log.d("robot", "robot in" + x + y);
                     map[x][y] = -1;
                     robot=true;
-                    fails.setVisibility(View.GONE);
+                    robotPlaceBtn.setVisibility(View.GONE);
                 }
                 break;
         }
         colorChooseDialog.hide();
+
     }
 
     public void exit(View view) {
         System.exit(0);
     }
 
-    public void createDialog() {
-        AlertDialog.Builder build;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            build = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
-        } else {
-            build = new AlertDialog.Builder(this);
-        }
-        build.setCancelable(false);
-        build.setTitle(R.string.alert_reconnect_title);
-        build.setMessage(R.string.alert_reconnect_text);
-        build.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                connect();
-            }
-        });
-        build.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                System.exit(0);
-            }
-        });
 
-        reconnectDialog = build.create();
-    }
-
-
-    public void startSearch(){
-        String message="&";
-        for(int i=0;i<5;i++){
-            for(int j=0;i<5;i++){
-                if(map[i][j]!=0){
-                    message=message+i+j+map[i][j]+"&";
+    public void startSearch() {
+        numberOfCells = 0;
+        String message = "&";
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; i < 5; i++) {
+                if (map[i][j] != 0) {
+                    message = message.concat(i + j + map[i][j] + "&");
+                    numberOfCells++;
                 }
             }
         }
+
         try {
-            Log.d("stringa",message);
+            Log.d("stringa", message);
             outputStream.write(message.getBytes(Charset.forName("UTF-8")));
             outputStream.write("\0".getBytes());
-        }catch(IOException e){
-            Log.d("errore","write error");
-        }
 
+            progressLayout.setVisibility(View.VISIBLE);
+            bar.setProgress(0);
+            bar.setMax(numberOfCells);
+
+            startReading();
+
+        } catch (IOException e) {
+            Log.d("errore", "write error");
+        }
     }
 
+    public void startReading(){
+        byte[] bytes = new byte[numberOfCells];
+        Boolean end = false;
+        int x,y,check;
+        checkedCells = 0;
+
+        while(!end){
+            try{
+                inputStream.read(bytes, 0, 3);
+                if(bytes[0] == END) {
+                    end = true;
+                    break;
+                }
+                x= bytes[0];
+                y= bytes[1];
+                check= bytes[2];
+                String id = "b" + x + y;
+                ImageButton b = findViewById(getResources().getIdentifier(id,null,null));
+
+                checkedCells ++;
+
+                b.setImageDrawable( getDrawable(check == CHECK ? R.drawable.ic_check : R.drawable.ic_clear));
+                bar.setProgress(checkedCells);
+
+            }catch (IOException e){
+                e.printStackTrace(); // AGGIUNGERE
+                Toast.makeText(this, "errore di ricezione", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void batteryInfo() {
+        // lunghezza byte DA MODIFICARE
+        int len = 2;
+        int batteryVoltage = 0;
+
+        /*
+        byte[] inputByte = new byte[len];
+
+        try {
+            inputStream.read(inputByte, 0, len);
+        } catch(IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Errore ricezione dati", Toast.LENGTH_SHORT).show();
+        }*/
+
+       if( batteryVoltage < VOLTAGE_LOW ){
+           batteryImage.setImageDrawable(getDrawable(R.drawable.ic_battery_20));
+       } else if( batteryVoltage < VOLTAGE_MIDDLE ){
+           batteryImage.setImageDrawable(getDrawable(R.drawable.ic_battery_50));
+       } else {
+           batteryImage.setImageDrawable(getDrawable(R.drawable.ic_battery_full));
+       }
+    }
 }
