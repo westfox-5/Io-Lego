d07c3dae950be617b9b6d92cb86ceb05b3cf1124
7h3-F1r3f0x <firefoxaxis@gmail.com>
2018-12-08 01:03:46 +0100

    added classes

---
+++ Android/IoLego2/app/src/main/java/com/iolego/io_lego/BluetoothConnection.java
@@ -0,0 +1,125 @@
+package com.iolego.io_lego;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.ParcelUuid;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.Set;
+
+public class BluetoothConnection extends Service {
+    private static final String TAG= "BLUETOOTH_ACTIVITY";
+    private static String MAC_ADDRESS = null;
+    private static final String ID_LEGO = "EV3";
+    private final IBinder bluetoothBinder = new BluetoothBinder();
+
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    private BluetoothSocket socket;
+
+
+    public class BluetoothBinder extends Binder {
+        public BluetoothConnection getService() { return BluetoothConnection.this; }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return bluetoothBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        // chiudi tutto
+        MAC_ADDRESS = null;
+        return super.onUnbind(intent);
+    }
+
+    boolean isConnected() {
+        return this.socket.isConnected();
+    }
+
+    int connect() throws IOException {
+        BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (blueAdapter != null) {
+            if (blueAdapter.isEnabled()) {/*ritorna true se il bluetooth è attivo sul telefono*/
+                if(MAC_ADDRESS != null) {
+
+                    socket.connect();
+                    Log.w(TAG, "Device connected. MAC " + MAC_ADDRESS);
+                    outputStream = socket.getOutputStream();
+                    inputStream = socket.getInputStream();
+
+                    return 0;
+
+                }else {
+                    Set<BluetoothDevice> bondedDevices = blueAdapter.getBondedDevices();
+                    BluetoothDevice device = searchDevice(bondedDevices);/*ritorna un bluetooth device*/
+                    if (device != null) {
+                        ParcelUuid[] uuids = device.getUuids();
+
+                        this.socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid());
+                        socket.connect();
+                        Log.w(TAG, "Device connected. MAC " + MAC_ADDRESS);
+                        outputStream = socket.getOutputStream();
+                        inputStream = socket.getInputStream();
+
+                        return 0;
+
+
+                    } else {
+                        Log.e(TAG, "No appropriate paired devices.");
+                    }
+                }
+            } else {
+                Log.e(TAG, "Bluetooth is disabled.");
+                return 2;
+            }
+        }
+        return 3;
+    }
+
+    private BluetoothDevice searchDevice(Set<BluetoothDevice> devices) {
+        for (BluetoothDevice device : devices) {
+            String deviceName = device.getName();
+            if (deviceName.equals(ID_LEGO)) {
+                MAC_ADDRESS = device.getAddress();
+                return device;
+            }
+        }
+        return null;
+    }
+
+
+    public void send(String str) throws IOException {
+        Log.d("stringa", str);
+        outputStream.write(str.getBytes(Charset.forName("UTF-8")));
+        outputStream.write("\0".getBytes());
+    }
+
+    String read() throws IOException {
+
+        InputStreamReader dataInputStream = new InputStreamReader(inputStream);
+        int bytesRead;
+        char[] buffer = new char[256];
+
+        bytesRead = dataInputStream.read(buffer, 0, 4);
+        return new String(buffer, 0, bytesRead);
+    }
+
+
+}
---
+++ Android/IoLego2/app/src/main/java/com/iolego/io_lego/SplashActivity.java
@@ -0,0 +1,224 @@
+package com.iolego.io_lego;
+
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final String TAG= "SPLASH_ACTIVITY";
+
+    private BluetoothConnection bt;
+    private Dialog reconnectDialog;
+    private TextView btConnectionTXT;
+    private ProgressBar bar;
+    private Handler barHandler;
+
+    private boolean bluetoothBound = false;
+
+
+    private Runnable hide_bar = new Runnable() {
+        @Override
+        public void run() {
+            bar.setVisibility(View.INVISIBLE);
+        }
+    };
+
+    private Runnable show_bar = new Runnable() {
+        @Override
+        public void run() {
+            bar.setIndeterminate(true);
+            bar.setVisibility(View.VISIBLE);
+        }
+    };
+
+
+
+
+    private ServiceConnection btServ = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            BluetoothConnection.BluetoothBinder binder = (BluetoothConnection.BluetoothBinder)iBinder;
+
+            bluetoothBound = true;
+            bt = binder.getService();
+
+            barHandler.post( show_bar );
+
+            new Handler().postDelayed(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            connect();
+                        }
+                    }, 500);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            bluetoothBound = false;
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startup);
+
+        btConnectionTXT = findViewById(R.id.connectTXT);
+        bar = findViewById(R.id.progressBar);
+
+        barHandler = new Handler();
+
+        reconnectDialog = createDialog();
+
+        Intent btIntet = new Intent(this, BluetoothConnection.class);
+        startService(btIntet);
+        bindService(btIntet, btServ, Context.BIND_AUTO_CREATE);
+
+
+    }
+
+    private void connect() {
+        if(reconnectDialog.isShowing()) reconnectDialog.dismiss();
+        int ris;
+        try {
+            ris = bt.connect();
+        } catch (IOException e) {
+            ris =-1;
+        }
+        switch(ris) {
+
+            case 0: // connesso
+                Log.d(TAG,"Connection established");
+                btConnectionTXT.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        btConnectionTXT.setText(getResources().getString(R.string.bt_connected));
+                    }
+                });
+
+                barHandler.removeCallbacks(show_bar);
+                barHandler.post(hide_bar);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+
+                        SplashActivity.this.finish();
+                        overridePendingTransition(R.anim.slide_left,R.anim.slide_right);
+
+                    }
+                }, 1000);
+
+                break;
+            case 1: // retry
+            case -1:
+                Log.d(TAG, "Reconnecting");
+
+                barHandler.removeCallbacks(show_bar);
+                barHandler.post(hide_bar);
+
+                reconnectDialog.show();
+                break;
+            case 2: // bt non acceso
+                btConnectionTXT.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        btConnectionTXT.setText(getResources().getString(R.string.bt_not_enable));
+                    }
+                });
+
+                barHandler.removeCallbacks(show_bar);
+                barHandler.post(hide_bar);
+
+                enableBluetooth();
+                break;
+        }
+    }
+
+
+    private AlertDialog createDialog() {
+        AlertDialog.Builder build;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            build = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            build = new AlertDialog.Builder(this);
+        }
+        build.setCancelable(false);
+        build.setTitle(R.string.alert_reconnect_title);
+        build.setMessage(R.string.alert_reconnect_text);
+        build.setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                new Handler().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        barHandler.removeCallbacks(hide_bar);
+                        barHandler.post(show_bar);
+                        connect();
+                    }
+                });
+            }
+        });
+        build.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                System.exit(0);
+            }
+        });
+
+        return build.create();
+    }
+
+    private void enableBluetooth() {
+        try {
+            Thread.sleep(500);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        Intent enabelBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(enabelBT,0 );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==0) {
+            Toast.makeText(this, getResources().getString(R.string.bt_enable), Toast.LENGTH_SHORT).show();
+            btConnectionTXT.setText(R.string.bt_pairing);
+            reconnectDialog.show();
+        } else {
+            Toast.makeText(this, getResources().getString(R.string.bt_error), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        if(btServ!=null) {
+            unbindService(btServ);
+        }
+        super.onStop();
+    }
+}
